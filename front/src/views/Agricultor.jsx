8c7e84b50380f399ffc7c0c6faf39c336ad67f5c
Antonio Fraga <am.fraga.led@gmail.com>
2024-07-09 18:29:27 +0200

    Fix after solve conflicts

--- front/src/views/Agricultor.jsx
+++ front/src/views/Agricultor.jsx
@@ -103,38 +103,7 @@ const AgricultorTrackingForm = () => {
             )}
         </div>
     );
-    return (
-        <div>
-            {isSubmitted ? (
-                <h1 style={{ textAlign: 'center', fontSize: '24px', fontWeight: 'bold', color: '#000000', marginTop:'24px'}}>🎉 Registro completado 🎉</h1>
-            ) : (
-                <form onSubmit={handleSubmit}>
-                    <div style={{ marginBottom: '20px' }}></div>
-                    <h1 style={{ textAlign: 'center', fontSize: '24px', fontWeight: 'bold', color: '#000000', marginBottom: '20px' }}>✍🏻 Añadir Información al Track ✍🏻</h1>
-                    <div style={{ marginBottom: '10px' }}>
-                        <label style={labelStyle}>Fecha:</label>
-                        <input type="date" name="date" value={formData.date} onChange={handleChange} required />
-                    </div>
-                    <div style={{ marginBottom: '10px' }}>
-                        <label style={labelStyle}>Ubicación:</label>
-                        <input type="text" name="location" value={formData.location} style={{ width: '300px' }} onChange={handleChange} required />
-                    </div>
-                    <div style={{ marginBottom: '10px' }}>
-                        <label style={labelStyle}>Cantidad:</label>
-                        <input type="number" name="quantity" value={formData.quantity} style={{ width: '75px' }} onChange={handleChange} required />
-                    </div>
-                    <div style={{ marginBottom: '10px' }}>
-                        <label style={labelStyle}>Información Adicional:</label>
-                        <input type="text" name="info" value={formData.info} style={{ width: '600px' }} onChange={handleChange} required />
-                    </div>
-                    <div style={{ marginBottom: '30px', justifyContent: 'center' }}></div>
-                    <div style={{ display: 'flex', justifyContent: 'center' }}>
-                        <button type="submit">Registrar</button>
-                    </div>
-                </form>
-            )}
-        </div>
-    );
+
 };
 
 export default AgricultorTrackingForm;
